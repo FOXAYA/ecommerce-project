94f5da7aa42c3474bb44c06205f1700dc68d4e31
FOX <75582176+FOXAYA@users.noreply.github.com>
2022-07-21 23:57:03 +0200

    commit forms

--- js/main.js
+++ js/main.js
@@ -43,3 +43,11 @@ window.onclick = function(event) {
     }
 }
 
+var modal2 = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
+}
