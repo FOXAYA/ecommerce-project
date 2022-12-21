3bd331c9d3a8e6e2a16acef1dcb6f596ff0a283e
FOX <75582176+FOXAYA@users.noreply.github.com>
2022-12-21 14:17:58 -0800

    end sell form

--- js/main.js
+++ js/main.js
@@ -42,29 +42,29 @@ function goToSignup() {
 //codezz
 window.fbAsyncInit = function () {
   FB.init({
-      appId: 530024878920498,
-      status: true,
-      cookie: true,
-      xfbml: true
+    appId: 530024878920498,
+    status: true,
+    cookie: true,
+    xfbml: true
   });
   FB.getLoginStatus(function (response) {
-      if (response.status === 'connected') {
-          console.log('connected');
-          var userProfile = document.getElementById('user-profile');
-          document.getElementById('fbBtn').style.display = "none";
-          document.getElementById('id01').style.display = 'none'
-          userProfile.style.display = "block";
+    if (response.status === 'connected') {
+      console.log('connected');
+      var userProfile = document.getElementById('user-profile');
+      document.getElementById('fbBtn').style.display = "none";
+      document.getElementById('id01').style.display = 'none'
+      userProfile.style.display = "block";
 
-          FB.api('/me', { fields: 'id, name, email' }, function (response) {
-              userProfile.innerHTML = response.name +
-                  '<img src="https://graph.facebook.com/' +
-                  response.id + '/picture">';
+      FB.api('/me', { fields: 'id, name, email' }, function (response) {
+        userProfile.innerHTML = response.name +
+          '<img src="https://graph.facebook.com/' +
+          response.id + '/picture">';
 
-          });
+      });
 
-      } else {
-          console.log("no connected");
-      }
+    } else {
+      console.log("no connected");
+    }
   });
 };
 
@@ -73,7 +73,7 @@ window.fbAsyncInit = function () {
   var id = 'facebook-jssdk';
   var ref = doc.getElementsByTagName('script')[0];
   if (doc.getElementById(id)) {
-      return;
+    return;
   }
   script = doc.createElement('script');
   script.id = id;
@@ -84,89 +84,28 @@ window.fbAsyncInit = function () {
 
 function FbApp_Login() {
   FB.login(function (response) {
-      if (response.authResponse) {
-          console.log("user  loging in");
-          window.location.replace('https://foxovic.epizy.com');
-      }
-      else {
-          alert("Attempt of Login is Failed!");
-      }
- `` }, { scope: 'email,user_photos,publish_actions' });
-}
-/**/
-function filterFunction(that, event) {
-    let container, input, filter, li, input_val;
-    container = $(that).closest(".searchable");
-    input_val = container.find("input").val().toUpperCase();
-
-    if (["ArrowDown", "ArrowUp", "Enter"].indexOf(event.key) != -1) {
-        keyControl(event, container)
-    } else {
-        li = container.find("ul li");
-        li.each(function (i, obj) {
-            if ($(this).text().toUpperCase().indexOf(input_val) > -1) {
-                $(this).show();
-            } else {
-                $(this).hide();
-            }
-        });
-
-        container.find("ul li").removeClass("selected");
-        setTimeout(function () {
-            container.find("ul li:visible").first().addClass("selected");
-        }, 100)
+    if (response.authResponse) {
+      console.log("user  loging in");
+      window.location.replace('https://foxovic.epizy.com');
     }
-}
-
-function keyControl(e, container) {
-    if (e.key == "ArrowDown") {
-
-        if (container.find("ul li").hasClass("selected")) {
-            if (container.find("ul li:visible").index(container.find("ul li.selected")) + 1 < container.find("ul li:visible").length) {
-                container.find("ul li.selected").removeClass("selected").nextAll().not('[style*="display: none"]').first().addClass("selected");
-            }
-
-        } else {
-            container.find("ul li:first-child").addClass("selected");
-        }
-
-    } else if (e.key == "ArrowUp") {
-
-        if (container.find("ul li:visible").index(container.find("ul li.selected")) > 0) {
-            container.find("ul li.selected").removeClass("selected").prevAll().not('[style*="display: none"]').first().addClass("selected");
-        }
-    } else if (e.key == "Enter") {
-        container.find("input").val(container.find("ul li.selected").text()).blur();
-        onSelect(container.find("ul li.selected").text())
+    else {
+      alert("Attempt of Login is Failed!");
     }
-
-    container.find("ul li.selected")[0].scrollIntoView({
-        behavior: "smooth",
-    });
+    ``
+  }, { scope: 'email,user_photos,publish_actions' });
 }
+/***select menu ****************/
+$(document).ready(function () {
 
-function onSelect(val) {
-    alert(val)
-}
+  // Initialize select2
+  $("#selUser").select2();
+  $("#selCoun").select2();
 
-$(".searchable input").focus(function () {
-    $(this).closest(".searchable").find("ul").show();
-    $(this).closest(".searchable").find("ul li").show();
-});
-$(".searchable input").blur(function () {
-    let that = this;
-    setTimeout(function () {
-        $(that).closest(".searchable").find("ul").hide();
-    }, 300);
-});
 
-$(document).on('click', '.searchable ul li', function () {
-    $(this).closest(".searchable").find("input").val($(this).text()).blur();
-    onSelect($(this).text())
+  // Read selected option
+  $('#but_read').click(function () {
+    var username = $('#selUser option:selected').text();
+    var userid = $('#selUser').val();
+  });
 });
-
-$(".searchable ul li").hover(function () {
-    $(this).closest(".searchable").find("ul li.selected").removeClass("selected");
-    $(this).addClass("selected");
-});
-/**/
+/***************************************** */
